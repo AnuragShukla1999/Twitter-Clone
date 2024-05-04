63603e20b6f60eeeacd768bf32bf7868854dd5ee
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-05-04 19:09:40 +0530

    .

---
+++ server/controllers/tweetController.js
@@ -0,0 +1,56 @@
+import { Tweet } from "../models/tweetSchema.js";
+import { User } from "../models/userSchema.js";
+
+
+export const createTweet = async (req, res) => {
+    try {
+        const { description, id } = req.body;
+
+        if (!description || !id) {
+            return res.status(401).json({
+                message: "Fields are required.",
+                success: false
+            });
+        };
+
+        const user = await User.findById(id).select("-password");
+
+        await Tweet.create({
+            description,
+            userId: id,
+            userDetails: user
+        });
+
+        return res.status(201).json({
+            message: "Tweet created successfully.",
+            success: true,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+export const deleteTweet = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Tweet.findByIdAndDelete(id);
+        return res.status(200).json({
+            message: "Tweet deleted successfully.",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+
+export const likeOrDislike = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
